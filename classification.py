6719fa4457661ebe3889a67932c49d2e7878d427
BenjaminIrwin <bti19@ic.ac.uk>
2020-01-28 15:17:40 +0000

    train nearly complete

--- classification.py
+++ classification.py
@@ -8,6 +8,7 @@
 ##############################################################################
 
 import numpy as np
+from numpy import ma
 
 
 class DecisionTreeClassifier(object):
@@ -26,67 +27,203 @@ class DecisionTreeClassifier(object):
     predict(X)
         Predicts the class label of samples X
     
-    """
-
+    """    
+    
+    
+    
     def __init__(self):
         self.is_trained = False
     
     
     def train(self, x, y):
         
-        length = len(x)
-        alphabet = np.array(["A","C","E","G","O","Q"])
-        alphabet_count = np.zeros((len(alphabet)))
-        alphabet_probabilty = np.zeros((len(alphabet)))
-        information = np.zeros((len(alphabet)))
-        
-        for indx,letter in enumerate(alphabet):
-            for index in range(0,length):
-                if(letter == y[index]):
-                    alphabet_count[indx]+=1
-                
-        alphabet_probabilty[:] = np.round(alphabet_count[:]/length,3)
-        #print(alphabet_probabilty)
-        information[0:] = alphabet_probabilty[0:] * np.log2(alphabet_probabilty[0:])
-        #test_info = alphabet_probabilty[0] * np.log2(alphabet_probabilty[0])
-        #test_info_2 = alphabet_probabilty[1] * np.log2(alphabet_probabilty[1])
-        #print(information)
-        root_entropy = -1*np.sum(information)
-        #print(root_entropy)
-        
-        """
-        Parameters
-        ----------
-        x : numpy.array
-            An N by K numpy array (N is the number of instances, K is the 
-            number of attributes)
-        y : numpy.array
-            An N-dimensional numpy array
-        
-        Returns
-        -------
-        DecisionTreeClassifier
-            A copy of the DecisionTreeClassifier instance
-        
-        """
-        
         # Make sure that x and y have the same number of instances
         assert x.shape[0] == len(y), \
             "Training failed. x and y must have the same number of instances."
         
+        #gets an index to sort
+        #index = [i for i in range(0,len(x))]
+        #x=np.insert(x,0,index,axis=1)
+        #y=x=np.insert(y,0,index,axis=1)
         
+        root_node = self.induce_decision_tree(x,y)
 
-        #######################################################################
-        #                 ** TASK 2.1: COMPLETE THIS METHOD **
-        #######################################################################
-        
-        
         
         # set a flag so that we know that the classifier has been trained
         self.is_trained = True
         
         return self
     
+    
+    
+    def induce_decision_tree(self,x,y):
+        
+        #Check whether they all equal the same thing
+        labels = np.unique(y)
+        length = len(labels)
+        if length == 1:
+            letter = self.terminal_leaf(y)
+            node = letter
+            return
+        
+        
+        
+        node = self.find_best_node(x,y)
+        child_1,child_2 = self.split_dataset(node)
+                
+        #Recursively call the function on the split dataset
+        #node["left"] = self.induce_decision_tree(child_1["attributes"],child_1["outcomes"])
+        #node["right"] = self.induce_decision_tree(child_2["attributes"],child_2["outcomes"])
+    
+        return node
+        
+    #Finds the propabilty of the class lables in the data set 
+    def find_probabilitys(self,x,y,class_labels,length):
+        
+     
+        alphabet,alphabet_count = np.unique(y,return_counts = True)
+        alphabet_count = np.array(alphabet_count,dtype = float)
+        
+        #Binary Classifcation
+        alphabet_probabilty = alphabet_count/length
+    
+        return alphabet_probabilty
+    
+    
+    #Finds the entropy of the dataset
+    def find_entropy(self,class_labels,probablity_matrix):
+        
+        information = np.zeros((len(class_labels)))
+                
+        ## need to deal with zeros
+        information = probablity_matrix * ma.log2(probablity_matrix)
+        information[information.mask] = 0
+        root_entropy = -1*np.sum(information)
+
+        return root_entropy
+    
+
+    def sort_dataset(self,x,y,col):
+        
+        length = len(y)
+        for j in range(0,length):
+            for j_2 in range(j,length):
+                if(x[j_2][col] < x[j][col]):
+                    
+                    #swapping 
+                    temp = x[j_2][:]
+                    temp_y = y[j_2]
+                    x[j_2][:] = x[j][:]
+                    x[j][:] = temp
+                    y[j_2] = y[j]
+                    y[j] = temp_y
+                    
+                    
+    def terminal_leaf(self,data_set):
+        labels,count = np.unique(data_set,return_counts = True)
+        index = np.argmax(count)
+        return (data_set[index])    
+    
+    def find_best_node(self,x,y):
+    
+        length,width = np.shape(x)
+        class_labels = np.unique(y)
+
+        class_labels_probabilty = self.find_probabilitys(x,y,class_labels,length)
+
+        root_entropy = self.find_entropy(class_labels,class_labels_probabilty)
+        previous_gain = 0
+        stored_col = 0
+        stored_value = 0
+        stored_attribute = 0
+        split_value = 5
+
+        for attribute in range(0,width):
+            
+            subset_1_x = []
+            subset_1_y = []
+            subset_2_x = []
+            subset_2_y = []
+            
+            for row in range(1,length):
+                
+                if x[row][attribute] < 5:
+                    
+                    subset_1_x.append(x[row][attribute])
+                    subset_1_y.append(y[row][0])
+                else:
+                    subset_2_x.append(x[row][attribute])
+                    subset_2_y.append(y[row][0])
+
+            outcomes_1 = np.unique(subset_1_y)
+            outcomes_2 = np.unique(subset_2_y)
+
+    
+            #get the probabliltys of each set
+            subset_1_prob = self.find_probabilitys(subset_1_x,subset_1_y,outcomes_1,len(subset_1_x))
+            subset_2_prob = self.find_probabilitys(subset_2_x,subset_2_y,outcomes_2,len(subset_2_y))
+            if len(subset_1_prob) != 0:
+                #get entropys of each set
+                subset_1_entropy = self.find_entropy(outcomes_1,subset_1_prob)
+            else:
+                subset_1_entropy = 0
+            
+            if len(subset_2_prob) != 0:    
+                subset_2_entropy = self.find_entropy(outcomes_2,subset_2_prob)
+            else:
+                subset_2_entropy = 0
+
+            subset_1_entropy_normalised = subset_1_entropy * len(subset_1_x)/length
+            subset_2_entropy_normalised = subset_2_entropy * len(subset_2_x)/length
+
+            #get total entropy
+            total_entropy = subset_1_entropy_normalised + subset_2_entropy_normalised
+            
+            #get gain
+            gain = root_entropy - total_entropy
+
+            #check whether it is bigger than the previous
+            if(gain > previous_gain):
+                stored_attribute = attribute
+                stored_value = split_value 
+                
+                # if not keep going 
+                # if so store the row and collumn and keep going
+                previous_gain = gain
+
+        #get the data into the node here
+        data = {"attributes":x,"outcomes":y}
+                
+        #returns the node 
+        return {"value":stored_value,"attribute":stored_attribute,"data":data,"left":None ,"right":None}
+    
+    def split_dataset(self,node):
+        
+        dataset = node["data"]
+        x = dataset["attributes"]
+        y = dataset["outcomes"]
+        attribute = node["attribute"]
+        split_value = node["value"]
+        left_x = []
+        right_x = []
+        left_y = []
+        right_y = []
+        
+        for row in range(0,len(x)):
+            
+            if x[row][attribute] < split_value:
+                left_x.append(x[row][:])
+                left_y.append(y[row][0])
+            else:
+                right_x.append(x[row][:])
+                right_y.append(y[row][0])
+                
+        left = {"attributes":np.array(left_x),"outcomes":np.array(left_y)}
+        right = {"attributes":np.array(right_x),"outcomes":np.array(right_y)}
+        
+        return (left,right)
+        
+
     
     def predict(self, x):
         
@@ -128,3 +265,19 @@ class DecisionTreeClassifier(object):
         return predictions
         
 
+                
+
+    
+    
+    def terminal_leaf(self,data_set):
+        labels,count = np.unique(data_set,return_counts = True)
+        index = np.argmax(count)
+        return data_set[index]
+        
+        
+        
+        
+            
+            
+            
+            
---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+from classification import DecisionTreeClassifier
+from eval import Evaluator
+
+if __name__ == "__main__":
+    
+    filename = "data/toy.txt"
+    data_set= np.loadtxt(filename,dtype=str,delimiter=',')
+    length = len(data_set)
+    line_length = len(data_set[0])
+    x = np.zeros((length,line_length-1))
+    y = np.zeros((length,1),dtype = str)
+        
+    for j in range(0,length):
+        for i in range(0,line_length):
+            if i < (line_length-1):
+                x[j][i] = data_set[j][i]
+            else:
+                y[j][0] = data_set[j][i]
+                
+    alphabet = np.array(["A","C","E","G","O","Q"])
+    alphabet_count = np.zeros((len(alphabet)))
+    alphabet_proportions = np.zeros((len(alphabet)))
+    for indx,letter in enumerate(alphabet):
+        for index in range(0,length):
+            if(letter == y[index]):
+                alphabet_count[indx]+=1
+                
+    alphabet_proportions[:] = np.round(alphabet_count[:]/length,3)
+    
+    print("Training the decision tree...")
+    classifier = DecisionTreeClassifier()
+    classifier = classifier.train(x,y)
+
+    print("Loading the test set...")
+    #predictions = classifier.predict(x)
+
+    
+    
+
